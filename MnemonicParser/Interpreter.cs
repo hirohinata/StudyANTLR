408e989cb58214968f73bb70409df018fad83486
hirohinata <azuri_hirohinata@hotmail.co.jp>
2019-08-25 21:11:20 +0900

    デバイス名の正規化処理追加

--- MnemonicParser/Interpreter.cs
+++ MnemonicParser/Interpreter.cs
@@ -13,5 +13,26 @@ namespace MnemonicParser
             var visitor = new MnemonicVisitor(plc);
             visitor.Visit(parser.input());
         }
+
+        public static string NormalizeBitDevice(string text)
+        {
+            MnemonicResult result = VisitOperand(text);
+            return (result as BitDeviceResult)?.ToString() ?? "";
+        }
+
+        public static string NormalizeWordDevice(string text)
+        {
+            MnemonicResult result = VisitOperand(text);
+            return (result as WordDeviceResult)?.ToString() ?? "";
+        }
+
+        private static MnemonicResult VisitOperand(string text)
+        {
+            var stream = new AntlrInputStream(text);
+            var lexer = new gen.MnemonicLexer(stream);
+            var parser = new gen.MnemonicParser(new CommonTokenStream(lexer));
+            var visitor = new MnemonicVisitor(null);
+            return visitor.Visit(parser.operand());
+        }
     }
 }
--- MnemonicParserTest/PlcTest.cs
+++ MnemonicParserTest/PlcTest.cs
@@ -11,6 +11,12 @@ namespace MnemonicParserTest
         private const bool ON = true;
         private const bool OFF = false;
 
+        private BitDevice GetBitDevice(Plc plc, string deviceText)
+            => plc.BitDevices[Interpreter.NormalizeBitDevice(deviceText)];
+
+        private WordDevice GetWordDevice(Plc plc, string deviceText)
+            => plc.WordDevices[Interpreter.NormalizeWordDevice(deviceText)];
+
         [Test, TestCaseSource(nameof(TestCaseSource))]
         public void Test(
             Dictionary<string, bool> bitDevices,
@@ -24,11 +30,11 @@ namespace MnemonicParserTest
             // 実行前のセットアップ
             foreach (var device in bitDevices)
             {
-                plc.BitDevices[device.Key].Value = device.Value;
+                GetBitDevice(plc, device.Key).Value = device.Value;
             }
             foreach (var device in wordDevices)
             {
-                plc.WordDevices[device.Key].Value = device.Value;
+                GetWordDevice(plc, device.Key).Value = device.Value;
             }
 
             // 構文解析＆意味解析してニモニックの実行
@@ -37,28 +43,28 @@ namespace MnemonicParserTest
             // 実行結果の確認
             foreach (var device in expectedBitDevices)
             {
-                Assert.AreEqual(device.Value, plc.BitDevices[device.Key].Value);
+                Assert.AreEqual(device.Value, GetBitDevice(plc, device.Key).Value);
             }
             foreach (var device in expectedWordDevices)
             {
-                Assert.AreEqual(device.Value, plc.WordDevices[device.Key].Value);
+                Assert.AreEqual(device.Value, GetWordDevice(plc, device.Key).Value);
             }
         }
 
         private static object[] TestCaseSource =
         {
             new object[] {
-                new Dictionary<string, bool> { { "R000", ON } },
+                new Dictionary<string, bool> { { "R0", ON } },
                 new Dictionary<string, ushort> { { "DM0", 10 }, { "DM1", 0 } },
                 "LD R0\nMOV DM0 DM1",
-                new Dictionary<string, bool> { { "R000", ON } },
+                new Dictionary<string, bool> { { "R0", ON } },
                 new Dictionary<string, ushort> { { "DM0", 10 }, { "DM1", 10 } }
             },
             new object[] {
-                new Dictionary<string, bool> { { "R000", OFF } },
+                new Dictionary<string, bool> { { "R0", OFF } },
                 new Dictionary<string, ushort> { { "DM0", 10 }, { "DM1", 0 } },
                 "LD R0\nMOV DM0 DM1",
-                new Dictionary<string, bool> { { "R000", OFF } },
+                new Dictionary<string, bool> { { "R0", OFF } },
                 new Dictionary<string, ushort> { { "DM0", 10 }, { "DM1", 0 } }
             },
         };
